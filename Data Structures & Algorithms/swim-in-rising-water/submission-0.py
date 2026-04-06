08bde17d4131b131c2ccbc3ba8543e50d80f8dac
Icker Villalon <icker.villalon@gmail.com>
2026-04-06 14:40:30 -0600

    Add: swim-in-rising-water - submission-0

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        """
+        Dijkstra's Algorithm
+        Time: O(n^2 * logn)
+        Space: O(n^2)
+        """
+        N = len(grid)
+        visit = set()
+        minH = [[grid[0][0], 0, 0]] # [time-maxheigth, row, col]
+        directions = [[0, 1], [0, -1], [-1, 0], [1, 0]]
+        visit.add((0, 0))
+        while minH:
+            t, r, c = heapq.heappop(minH)
+            if r == N - 1 and c == N - 1: return t
+            for dr, dc in directions:
+                neiR, neiC = r + dr, c + dc
+                if (neiR < 0 or neiC < 0 or
+                    neiR == N or neiC == N or
+                    (neiR, neiC) in visit): continue
+                visit.add((neiR, neiC))
+                heapq.heappush(minH, [max(t, grid[neiR][neiC]), neiR, neiC])
